f43a9ff909f843831e0cc9c32f07608d9de768f8
Levin Rickert <me@levrik.io>
2017-07-05 22:05:24 +0200

    Modernized build script

--- scripts/generate.js
+++ scripts/generate.js
@@ -1,41 +1,34 @@
 const fs = require('fs');
+const pathRegex = /\sd="(.*)"/;
 
-const template =
+const svgFiles = fs.readdirSync(`${__dirname}/../mdi/icons/svg`);
+for (let svgFile of svgFiles) {
+  const name = svgFile.split(/-/g).map(part => {
+    return part.charAt(0).toUpperCase() + part.slice(1);
+  }).join('').slice(0, -4);
+
+  const content = fs.readFileSync(`${__dirname}/../mdi/icons/svg/${svgFile}`);
+  const pathMatches = pathRegex.exec(content);
+  const path = pathMatches && pathMatches[1];
+  // Skip on empty path
+  if (!path) continue;
+
+  const fileContent =
 `import React from 'react';
 
-const $NAME$Icon = ({ width = 24, height = 24, viewBox = '0 0 24 24', className, children, ...props }) => {
+const ${name}Icon = ({ width = 24, height = 24, viewBox = '0 0 24 24', className, children, ...props }) => {
   let classes = 'mdi-icon';
   if (className) classes += \` \${className}\`;
 
   return (
     <svg {...props} width={width} height={height} viewBox={viewBox} className={classes}>
-      <path d="$PATH$" />
+      <path d="${path}" />
     </svg>
   );
 };
 
-export default $NAME$Icon;
+export default ${name}Icon;
 `;
 
-const pathRegex = /\sd="(.*)"/;
-const svgs = fs.readdirSync(`${__dirname}/../mdi/icons/svg`);
-const components = svgs.map(svg => {
-  return {
-    name: svg.split(/-/g).map(part => {
-      return part.charAt(0).toUpperCase() + part.slice(1);
-    }).join('').slice(0, -4),
-    path: (() => {
-      const content = fs.readFileSync(`${__dirname}/../mdi/icons/svg/${svg}`, { encoding: 'utf8' });
-
-      let pathMatches = pathRegex.exec(content);
-      if (pathMatches) {
-        return pathMatches[1];
-      }
-    })()
-  };
-}).filter(component => component.path);
-
-for (let component of components) {
-  let fileContent = template.replace(/\$NAME\$/g, component.name).replace(/\$PATH\$/g, component.path);
-  fs.writeFileSync(`${__dirname}/../build/${component.name}Icon.js`, fileContent);
+  fs.writeFileSync(`${__dirname}/../build/${name}Icon.js`, fileContent);
 }
