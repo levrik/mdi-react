55eaee0a81d14451784a46fa68730957f7c8bf30
Levin Rickert <me@levrik.io>
2019-11-30 18:18:22 +0100

    Update typings tests

--- typings-test-preact.tsx
+++ typings-test-preact.tsx
@@ -1,20 +1,13 @@
 import AccessPointIcon from './publish-preact/AccessPointIcon';
-import { AccessPointNetworkIcon } from './publish-preact';
 
 // this will throw a type error
 const noRender = () => (
   <>
     <AccessPointIcon>Things that are not shown</AccessPointIcon>
-    <AccessPointIcon color={000} />
+    <AccessPointIcon color={0} />
     <AccessPointIcon size />
     <AccessPointIcon class={{ 'some-class': true }} />
     <AccessPointIcon className={{ 'some-class': true }} />
-
-    <AccessPointNetworkIcon>Things that are not shown</AccessPointNetworkIcon>
-    <AccessPointNetworkIcon color={000} />
-    <AccessPointNetworkIcon size />
-    <AccessPointIcon class={{ 'some-class': true }} />
-    <AccessPointNetworkIcon className={{ 'some-class': true }} />
   </>
 );
 
@@ -24,11 +17,7 @@ const render = () => (
     <AccessPointIcon></AccessPointIcon>
     <AccessPointIcon size={16} />
     <AccessPointIcon color="#fff" size="16" class="some-class" className="some-class" />
-
-    <AccessPointNetworkIcon></AccessPointNetworkIcon>
-    <AccessPointNetworkIcon size={16} />
-    <AccessPointNetworkIcon color="#fff" size="16" class="some-class" className="some-class" />
-    <AccessPointNetworkIcon
+    <AccessPointIcon
       size={16} style={{display: 'block', margin: 'auto'}}
       onClick={() => {}} />
   </>
--- typings-test-react.tsx
+++ typings-test-react.tsx
@@ -1,18 +1,12 @@
 import AccessPointIcon from './publish-react/AccessPointIcon';
-import { AccessPointNetworkIcon } from './publish-react';
 
 // this will throw a type error
 const noRender = () => (
   <>
     <AccessPointIcon>Things that are not shown</AccessPointIcon>
-    <AccessPointIcon color={000} />
+    <AccessPointIcon color={0} />
     <AccessPointIcon size />
     <AccessPointIcon className={5} />
-
-    <AccessPointNetworkIcon>Things that are not shown</AccessPointNetworkIcon>
-    <AccessPointNetworkIcon color={000} />
-    <AccessPointNetworkIcon size />
-    <AccessPointNetworkIcon className={5} />
   </>
 );
 
@@ -22,11 +16,7 @@ const render = () => (
     <AccessPointIcon></AccessPointIcon>
     <AccessPointIcon size={16} />
     <AccessPointIcon color="#fff" size="16" className="some-class" />
-
-    <AccessPointNetworkIcon></AccessPointNetworkIcon>
-    <AccessPointNetworkIcon size={16} />
-    <AccessPointNetworkIcon color="#fff" size="16" className="some-class" />
-    <AccessPointNetworkIcon
+    <AccessPointIcon
       size={16} style={{display: 'block', margin: 'auto'}}
       onClick={() => {}} />
   </>
